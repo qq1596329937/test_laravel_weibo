1a82598818a760067ff6b20ea729482df44000a8
2937927783@qq.com <a19880614a>
2020-04-27 14:15:30 +0800

    添加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,4 +17,16 @@ class UsersController extends Controller
     }
 
 
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
+
+
+
 }
