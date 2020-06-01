ce416576450d16d26d3b991054f1d78bd619d0e0
2937927783@qq.com <a19880614a>
2020-06-01 09:52:33 +0800

    关注用户动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,10 +59,12 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
             ->orderBy('created_at', 'desc');
     }
-
     public function followers()
     {
         return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
@@ -92,4 +94,5 @@ class User extends Authenticatable
     {
         return $this->followings->contains($user_id);
     }
+
 }
